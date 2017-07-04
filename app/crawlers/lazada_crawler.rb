75a19c60f355877785290f613c2806fee8fdab0b
Hoang Phuc <hoangphucd3@gmail.com>
2017-07-04 23:25:11 +0700

    Update Lazada Crawler

--- app/crawlers/lazada_crawler.rb
+++ app/crawlers/lazada_crawler.rb
@@ -4,11 +4,15 @@ class LazadaCrawler < Crawler::CrawlerBase
   BASE_URL = 'http://www.lazada.vn'
 
   def crawl
+    valid_product_file_name = "data-holder/lazada/product_#{category.underscore}.csv"
+    output_file = CSV.open(valid_product_file_name, 'w')
+    output_file << ['product_title', 'product_url', 'product_image', 'product_price', 'product_details']
+
     # Get meta info from first page
     first_page = agent.get("#{url}/#{category}/?itemperpage=120")
 
     last_page_element = parser::HTML(first_page).at_xpath("//div[@class='c-paging']//a[@class='c-paging__link'][last()]//@href")
-    last_page = last_page_element.value.scan(/(?<=&page=)\d+/i)[0].to_i
+    last_page = last_page_element.value.scan(/(?<=[?|\&]page=)\d+/i)[0].to_i
 
     (1..last_page).each do |page_number|
       page = agent.get("#{url}/#{category}/?itemperpage=120&page=#{page_number}")
@@ -18,30 +22,26 @@ class LazadaCrawler < Crawler::CrawlerBase
         json_ld = JSON.parse(node.text)
         next if json_ld['@type'] != 'ItemList'
         products = json_ld['itemListElement']
-        valid_product_file_name = "data-holder/lazada/product_#{category.underscore}.csv"
 
-        CSV.open(valid_product_file_name, 'a+') do |csv|
-          csv << ['product_title', 'product_url', 'product_image', 'product_price', 'product_details']
-          products.each do |product|
-            # https://www.reddit.com/r/ruby/comments/3nvvhk/help_stumped_dealing_with_embedded_quotes_in_csv/?st=j4nqx8et&sh=bc62e42f
-            product_name = product['name'].gsub(/[\n(?<!^|,)"(?!,|$)]/, ' ')
+        products.each do |product|
+          # https://www.reddit.com/r/ruby/comments/3nvvhk/help_stumped_dealing_with_embedded_quotes_in_csv/?st=j4nqx8et&sh=bc62e42f
+          product_name = product['name'].gsub(/[\n(?<!^|,)"(?!,|$)]/, ' ')
 
-            log '=' * 100
-            log "Processing #{product_name} - Page: #{page_number}/#{last_page}".yellow
+          log '=' * 100
+          log "Processing #{product_name} - Page: #{page_number}/#{last_page}".yellow
 
-            product_url = product['url'].gsub(/(\?.+)/, '')
-            product_price = (product['offers']['price'].to_i)/22000
+          product_url = product['url'].gsub(/(\?.+)/, '')
+          product_price = (product['offers']['price'].to_i)/22000
 
-            product_detail_elements = agent.get(product_url)
-            product_desc = parser::HTML(product_detail_elements).css('#productDetails').text
-            product_image_element = parser::HTML(product_detail_elements).css('#productZoom')
-            product_image = product_image_element.present? ? product_image_element[0]['data-zoom-image'] : ''
+          product_detail_elements = agent.get(product_url)
+          product_desc = parser::HTML(product_detail_elements).css('#productDetails').text
+          product_image_element = parser::HTML(product_detail_elements).css('#productZoom')
+          product_image = product_image_element.present? ? product_image_element[0]['data-zoom-image'] : ''
 
-            csv << [product_name, product_url, product_image, product_price, product_desc]
+          output_file << [product_name, product_url, product_image, product_price, product_desc]
 
-            log "Saved #{product_name} - Page: #{page_number}/#{last_page}".yellow
-            log '=' * 100
-          end
+          log "Saved #{product_name} - Page: #{page_number}/#{last_page}".yellow
+          log '=' * 100
         end
       end
     end
